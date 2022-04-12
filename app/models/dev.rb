35d2207cda11236f622784acbe13c5b6595223b1
Allison <amh03160@gmail.com>
2022-04-12 10:09:51 -0500

    core deliverables finished

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,18 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def recieved_one?(item_name)
+        contains_item = self.freebies.filter{|freebie| freebie.item_name == item_name}
+        if(contains_item.length > 0)
+            true else false
+        end 
+    end
+
+    def give_away(dev, freebie)
+        if(freebie.dev_id == self.id)
+           freebie.update(dev_id: dev.id)
+        else puts "you can't give me away"
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,35 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+Freebie.create(item_name:"Jim Bobblehead",value:5,dev_id:1,company_id:1)
+Freebie.create(item_name:"Rocco Bobblehead",value:3,dev_id:2,company_id:1)
+Freebie.create(item_name:"James Bobblehead",value:3,dev_id:3,company_id:1)
+Freebie.create(item_name:"Jack Bobblehead",value:3,dev_id:4,company_id:1)
+Freebie.create(item_name:"Taika Waititi Bobblehead",value:5,dev_id:5,company_id:1)
 
+Freebie.create(item_name:"Frisbee",value:3,dev_id:2,company_id:2)
+Freebie.create(item_name:"Green Frisbee",value:3,dev_id:1,company_id:2)
+Freebie.create(item_name:"Blue Frisbee",value:3,dev_id:3,company_id:2)
+Freebie.create(item_name:"Purple Frisbee",value:3,dev_id:4,company_id:2)
+Freebie.create(item_name:"Green Frisbee",value:3,dev_id:5,company_id:2)
+
+Freebie.create(item_name:"pen",value:1,dev_id:1,company_id:3)
+Freebie.create(item_name:"pen",value:1,dev_id:2,company_id:3)
+Freebie.create(item_name:"pen",value:1,dev_id:3,company_id:3)
+Freebie.create(item_name:"pen",value:1,dev_id:4,company_id:3)
+Freebie.create(item_name:"pen",value:1,dev_id:5,company_id:3)
+
+Freebie.create(item_name:"fan",value:1,dev_id:1,company_id:4)
+Freebie.create(item_name:"fan",value:1,dev_id:2,company_id:4)
+Freebie.create(item_name:"fan",value:1,dev_id:3,company_id:4)
+Freebie.create(item_name:"fan",value:1,dev_id:4,company_id:4)
+Freebie.create(item_name:"fan",value:1,dev_id:5,company_id:4)
+
+Freebie.create(item_name:"squishmallow",value:40,dev_id:1,company_id:5)
+Freebie.create(item_name:"squishmallow",value:40,dev_id:2,company_id:5)
+Freebie.create(item_name:"squishmallow",value:40,dev_id:3,company_id:5)
+Freebie.create(item_name:"squishmallow",value:40,dev_id:4,company_id:5)
+Freebie.create(item_name:"squishmallow",value:40,dev_id:5,company_id:5)
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
